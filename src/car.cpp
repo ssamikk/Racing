184dfddb7a72286eb6f99854379db8f6fbd0e6c7
samoylenko <ssamikk@mail.ru>
2023-01-16 20:50:22 +0400

    Signed-off-by: samoylenko <ssamikk@mail.ru>

---
+++ src/car.cpp
@@ -0,0 +1,39 @@
+#include "car.h"
+
+Car::Car(int _num_checkpoints, const int _points[8][2])
+    : speed(2), angle(0), n(0)
+{
+    num_checkpoints = _num_checkpoints;
+    for(int i = 0; i < num_checkpoints; ++i)
+    {
+        for(int j = 0; j < 2; ++j)
+        {
+            points[i][j] = _points[i][j];
+        }
+    }
+}
+
+void Car::move()
+{
+    x += sin(angle) * speed;
+    y -= cos(angle) * speed;
+}
+
+void Car::findTarget()
+{
+    float tx= points[n][0];
+    float ty= points[n][1];
+    float beta = angle-atan2(tx-x,-ty+y);
+    if (sin(beta)<0)
+    {
+        angle+=0.005*speed;
+    }
+    else
+    {
+        angle-=0.005*speed;
+    }
+    if ((x-tx)*(x-tx)+(y-ty)*(y-ty)<25*25)
+    {
+        n=(n+1) % num_checkpoints;
+    }
+}
